4a54e2d9e5318582b8a0a6c71b67b9feea967797
EC2 Default User <ec2-user@ip-172-31-17-172.us-east-2.compute.internal>
2020-05-01 00:51:05 +0000

    circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -11,9 +11,15 @@ kubectl create -f kubernetes/${pod_name}.yml
 # List kubernetes pods
 kubectl get pods
 
+
+POD_STATUS=$(kubectl get pods | egrep flask-app| awk '{print $3}' )
+while [ "$POD_STATUS" == "Pending" ]; do
+echo "waiting be active"
+sleep 5
+done
 # Waiting for 10 secs in order to pod turns into "Running" state
 # before forwarding.
-# sleep 10
+#sleep 10
 
 # Forward the container port to a host
 kubectl port-forward ${pod_name} 8000:80
